c79003478eacf9e1d67096e82d9dc99836f813a5
Chien-Ta Wu <sleepgo@gmail.com>
2013-03-08 04:53:20 +0800

    update wq.sh: tumblr add 2 patterns

--- wq.sh
+++ wq.sh
@@ -26,18 +26,44 @@ while [ ! -z "$1" ]; do
     ${WGET} -nv -4 --no-check-certificate "$1"
 
     # pattern: tumblr image
-    # url for example: http://24.media.tumblr.com/tumblr_lp2h0dKA8s1qzn3jqo1_500.jpg
+    # url pattern:
+    #  1. http://25.media.tumblr.com/409bc297bb66a375ac2c85e78d0e387e/tumblr_miu9svkSvK1qk5m6io8_250.jpg
+    #  2. http://24.media.tumblr.com/tumblr_kt065fJHvm1qzkcgao1_r1_1280.jpg
+    #  3. http://24.media.tumblr.com/tumblr_lp2h0dKA8s1qzn3jqo1_500.jpg
+    #  4. TBD
+
     TUMBLRCOUNT=`echo "$1" | ${GREP} '[0-9].media.tumblr.com/' | ${WC} -l | ${TR} -d ' '`
     if [ $TUMBLRCOUNT -gt 0 ]; then
         HOSTNAME=`echo "$1" | ${AWK} -F/ '{printf("%s",$3);}'`
-        FILENAME=`echo "$1" | ${AWK} -F/ '{printf("%s",$4);}'`
-        FILENAMEBODY=`echo "${FILENAME}" | ${AWK} -F_ '{printf("%s_%s",$1,$2);}'`
+
+        # check pattern 1
+        FILENAME=`echo "$1" | ${AWK} -F/ '{printf("%s",$5);}'`
+        if [ -z "${FILENAME}" ]; then
+            # this is pattern 2 or 3
+            FILENAME=`echo "$1" | ${AWK} -F/ '{printf("%s",$4);}'`
+            PATH=''
+        else
+            # this is pattern 1
+            PATH=`echo "$1" | ${AWK} -F/ '{printf("%s",$4);}'`
+            PATH=`echo -n "${PATH}/"`
+        fi
+
+        # check pattern 2
+        FILENAMESIZE=`echo "${FILENAME}" | ${AWK} -F_ '{printf("%s",$4);}'`
+        if [ -z "${FILENAMESIZE}" ]; then
+            # this is pattern 3
+            FILENAMEBODY=`echo "${FILENAME}" | ${AWK} -F_ '{printf("%s_%s",$1,$2);}'`
+        else
+            # this is pattern 2
+            FILENAMEBODY=`echo "${FILENAME}" | ${AWK} -F_ '{printf("%s_%s_%s",$1,$2,$3);}'`
+        fi
+
         FILENAMEEXT=`echo "${FILENAME}" | ${AWK} -F. '{printf("%s",$2);}'`
 
         # try get bigger size: 1280 1024 800 600
         # maybe there are more bigger size in the future
         for SIZE in 1280 1024 800 600; do
-            URL=`echo -n "http://${HOSTNAME}/${FILENAMEBODY}_${SIZE}.${FILENAMEEXT}"`
+            URL=`echo -n "http://${HOSTNAME}/${PATH}${FILENAMEBODY}_${SIZE}.${FILENAMEEXT}"`
             LOCALFILENAME=`echo -n "${FILENAMEBODY}_${SIZE}.${FILENAMEEXT}"`
 
             ${WGET} -4 -nv --no-check-certificate "${URL}"
