e99c23a41a15613c9728fb390c14cf4aa277a088
Chien-Ta Wu <sleepgo@gmail.com>
2013-06-05 03:08:38 +0800

    update wq.sh: if url is pixiv image, fake refererer from pixiv webpage url

--- wq/wq.sh
+++ wq/wq.sh
@@ -23,9 +23,15 @@ WGETOPTION="-nv -4 --no-check-certificate"
 WGETUSERAGENT="Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-TW; rv:1.9.2.24) Gecko/20111103 Firefox/3.6.24 (.NET CLR 3.5.30729)"  # pretend windows browsers
 
 # referer default none
-WGETREFERER=""
+CLEANREFERER=1
 
 while [ ! -z "$1" ]; do
+    if [ ${CLEANREFERER} -gt 0 ]; then
+        WGETREFERER=""
+    fi
+
+    CLEANREFERER=0
+
     # avoid double-typed command
     if [ "wq" == "$1" -o "wq.sh" == "$1" -o "$0" == "$1" ]; then
         shift
@@ -46,6 +52,35 @@ while [ ! -z "$1" ]; do
     CHECKSLASHSLASH=`echo "$1" | ${CUT} -c 1-2`
     if [ "//" = "${CHECKSLASHSLASH}" ]; then
         URL=`echo -n "http:$1"`
+    fi
+
+    # parse url
+    HOSTNAME=`echo "${URL}" | ${AWK} -F/ '{printf("%s",$3);}'`
+    HOSTNAMEA=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$1);}'`
+    HOSTNAMEB=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$2);}'`
+    HOSTNAMEC=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$3);}'`
+    HOSTNAMED=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$4);}'`
+    PATHA=`echo "${URL}" | ${AWK} -F/ '{printf("%s",$4);}'`
+    PATHB=`echo "${URL}" | ${AWK} -F/ '{printf("%s",$5);}'`
+    PATHC=`echo "${URL}" | ${AWK} -F/ '{printf("%s",$6);}'`
+    PATHD=`echo "${URL}" | ${AWK} -F/ '{printf("%s",$7);}'`
+
+    # pixiv: get pixture with faked referer
+    #   url: http://i2.pixiv.net/img20/img/stargeyser/10931186.jpg?1277014586
+    #   ref: http://www.pixiv.net/member_illust.php?mode=big&illust_id=10931186
+
+    if [ 'pixiv' == "${HOSTNAMEB}" -a 'net' == "${HOSTNAMEC}" ]; then
+        FILENAME=`echo "${PATHD}" | ${AWK} -F? '{printf("%s",$1);}'`
+        FILEID=`echo "${FILENAME}" | ${AWK} -F. '{printf("%d",$1);}'`
+        ARGS=`echo "${PATHD}" | ${AWK} -F? '{printf("%s",$2);}'`
+
+        # if url has ? , remove all after ?
+        if [ ! -z "${ARGS}" ]; then
+            URL=`echo "${URL}" | ${AWK} -F? '{printf("%s",$1);}'`
+        fi
+
+        WGETREFERER="--referer=http://www.pixiv.net/member_illust.php?mode=big&illust_id=${FILEID}"
+        CLEANREFERER=1
     fi
 
     # get file first
