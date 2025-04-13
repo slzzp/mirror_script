e6a0fc4c377324dff0de3454c5f1b1f53dc45a01
Slzzp Wu <sleepgo@gmail.com>
2025-04-14 06:37:36 +0800

    update parse_url.sh: use ECHO

--- parse_url.sh
+++ parse_url.sh
@@ -13,12 +13,12 @@ DEBUGMSG=0
 . ~/work/mirror_script/cmd.sh
 
 # preprocess url
-URL=`echo -n "$1"`
+URL=`${ECHO} -n "$1"`
 
 # check if $1's prefix is '//'
-CHECKSLASHSLASH=`echo "$1" | ${CUT} -c 1-2`
+CHECKSLASHSLASH=`${ECHO} "$1" | ${CUT} -c 1-2`
 if [ "//" = "${CHECKSLASHSLASH}" ]; then
-    URL=`echo -n "http:$1"`
+    URL=`${ECHO} -n "http:$1"`
 fi
 
 
@@ -27,36 +27,35 @@ fi
 #   %2F -> /
 #   %3D -> =
 #   %3F -> ?
-PARSEURL=`echo -n "${URL}" | ${SED} 's/%26/\&/g' | ${SED} 's/%2F/\//g' | ${SED} 's/%3D/\=/g' | ${SED} 's/%3F/\?/g'`
-
+PARSEURL=`${ECHO} -n "${URL}" | ${SED} 's/%26/\&/g' | ${SED} 's/%2F/\//g' | ${SED} 's/%3D/\=/g' | ${SED} 's/%3F/\?/g'`
 
 # parse url
-PROTOCOL=`echo "${PARSEURL}" | ${AWK} -F: '{printf("%s",$1);}'`
+PROTOCOL=`${ECHO} "${PARSEURL}" | ${AWK} -F: '{printf("%s",$1);}'`
 
-HOSTNAME=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$3);}'`
+HOSTNAME=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$3);}'`
 
-HOSTNAMEA=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$1);}'`
-HOSTNAMEB=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$2);}'`
-HOSTNAMEC=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$3);}'`
-HOSTNAMED=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$4);}'`
-HOSTNAMEE=`echo "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$5);}'`
+HOSTNAMEA=`${ECHO} "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$1);}'`
+HOSTNAMEB=`${ECHO} "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$2);}'`
+HOSTNAMEC=`${ECHO} "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$3);}'`
+HOSTNAMED=`${ECHO} "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$4);}'`
+HOSTNAMEE=`${ECHO} "${HOSTNAME}" | ${AWK} -F. '{printf("%s",$5);}'`
 
-PATHA=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$4);}'`
-PATHB=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$5);}'`
-PATHC=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$6);}'`
-PATHD=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$7);}'`
-PATHE=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$8);}'`
-PATHF=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$9);}'`
-PATHG=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$10);}'`
-PATHH=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$11);}'`
-PATHI=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$12);}'`
-PATHJ=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$13);}'`
-PATHK=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$14);}'`
-PATHL=`echo "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$15);}'`
+PATHA=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$4);}'`
+PATHB=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$5);}'`
+PATHC=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$6);}'`
+PATHD=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$7);}'`
+PATHE=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$8);}'`
+PATHF=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$9);}'`
+PATHG=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$10);}'`
+PATHH=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$11);}'`
+PATHI=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$12);}'`
+PATHJ=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$13);}'`
+PATHK=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$14);}'`
+PATHL=`${ECHO} "${PARSEURL}" | ${AWK} -F/ '{printf("%s",$15);}'`
 
 FILENAMEARG=`${BASENAME} "${PARSEURL}"`
-FILENAME=`echo "${FILENAMEARG}" | ${AWK} -F? '{printf("%s",$1);}'`
-FILENAMEEXT=`echo "${FILENAME}" | ${AWK} -F. '{printf("%s", $NF);}'`
+FILENAME=`${ECHO} "${FILENAMEARG}" | ${AWK} -F? '{printf("%s",$1);}'`
+FILENAMEEXT=`${ECHO} "${FILENAME}" | ${AWK} -F. '{printf("%s", $NF);}'`
 
 if [ "${FILENAME}" = "${FILENAMEEXT}" ]; then
   FILENAMEEXT=''
@@ -67,66 +66,66 @@ FILENAMEEXTLEN=${#FILENAMEEXT}
 FILENAMEMAINLEN=`${EXPR} ${FILENAMELEN} - ${FILENAMEEXTLEN} - 1`
 
 if [ ${FILENAMEEXTLEN} -gt 0 ]; then
-  FILENAMEMAIN=`echo "${FILENAME}" | ${CUT} -c 1-${FILENAMEMAINLEN}`
+  FILENAMEMAIN=`${ECHO} "${FILENAME}" | ${CUT} -c 1-${FILENAMEMAINLEN}`
 else
-  FILENAMEMAIN=`echo -n "${FILENAME}"`
+  FILENAMEMAIN=`${ECHO} -n "${FILENAME}"`
 fi
 
-FILENAMEMAINFBA=`echo "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$1);}'`
-FILENAMEMAINFBB=`echo "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$2);}'`
-FILENAMEMAINFBC=`echo "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$3);}'`
-FILENAMEMAINFBD=`echo "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$4);}'`
+FILENAMEMAINFBA=`${ECHO} "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$1);}'`
+FILENAMEMAINFBB=`${ECHO} "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$2);}'`
+FILENAMEMAINFBC=`${ECHO} "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$3);}'`
+FILENAMEMAINFBD=`${ECHO} "${FILENAMEMAIN}" | ${AWK} -F_ '{printf("%s",$4);}'`
 
-ARGS=`echo "${FILENAMEARG}" | ${AWK} -F? '{printf("%s",$2);}'`
+ARGS=`${ECHO} "${FILENAMEARG}" | ${AWK} -F? '{printf("%s",$2);}'`
 
-ARGA=`echo "${ARGS}" | ${AWK} -F\& '{printf("%s",$1);}'`
-ARGAN=`echo "${ARGA}" | ${AWK} -F= '{printf("%s",$1);}'`
-ARGAV=`echo "${ARGA}" | ${AWK} -F= '{printf("%s",$2);}'`
+ARGA=`${ECHO} "${ARGS}" | ${AWK} -F\& '{printf("%s",$1);}'`
+ARGAN=`${ECHO} "${ARGA}" | ${AWK} -F= '{printf("%s",$1);}'`
+ARGAV=`${ECHO} "${ARGA}" | ${AWK} -F= '{printf("%s",$2);}'`
 
-ARGB=`echo "${ARGS}" | ${AWK} -F\& '{printf("%s",$2);}'`
-ARGBN=`echo "${ARGB}" | ${AWK} -F= '{printf("%s",$1);}'`
-ARGBV=`echo "${ARGB}" | ${AWK} -F= '{printf("%s",$2);}'`
+ARGB=`${ECHO} "${ARGS}" | ${AWK} -F\& '{printf("%s",$2);}'`
+ARGBN=`${ECHO} "${ARGB}" | ${AWK} -F= '{printf("%s",$1);}'`
+ARGBV=`${ECHO} "${ARGB}" | ${AWK} -F= '{printf("%s",$2);}'`
 
-ARGC=`echo "${ARGS}" | ${AWK} -F\& '{printf("%s",$3);}'`
-ARGCN=`echo "${ARGC}" | ${AWK} -F= '{printf("%s",$1);}'`
-ARGCV=`echo "${ARGC}" | ${AWK} -F= '{printf("%s",$2);}'`
+ARGC=`${ECHO} "${ARGS}" | ${AWK} -F\& '{printf("%s",$3);}'`
+ARGCN=`${ECHO} "${ARGC}" | ${AWK} -F= '{printf("%s",$1);}'`
+ARGCV=`${ECHO} "${ARGC}" | ${AWK} -F= '{printf("%s",$2);}'`
 
 
 # debug output
 if [ ${DEBUGMSG} -gt 0 ]; then
-    echo "URL='${URL}'"
-    echo "PROTOCOL='${PROTOCOL}'"
+    ${ECHO} "URL='${URL}'"
+    ${ECHO} "PROTOCOL='${PROTOCOL}'"
 
-    echo "HOSTNAME='${HOSTNAME}'"
-    echo "HOSTNAMEA='${HOSTNAMEA}'"
-    echo "HOSTNAMEB='${HOSTNAMEB}'"
-    echo "HOSTNAMEC='${HOSTNAMEC}'"
-    echo "HOSTNAMED='${HOSTNAMED}'"
-    echo "HOSTNAMEE='${HOSTNAMEE}'"
+    ${ECHO} "HOSTNAME='${HOSTNAME}'"
+    ${ECHO} "HOSTNAMEA='${HOSTNAMEA}'"
+    ${ECHO} "HOSTNAMEB='${HOSTNAMEB}'"
+    ${ECHO} "HOSTNAMEC='${HOSTNAMEC}'"
+    ${ECHO} "HOSTNAMED='${HOSTNAMED}'"
+    ${ECHO} "HOSTNAMEE='${HOSTNAMEE}'"
 
-    echo "PATHA='${PATHA}'"
-    echo "PATHB='${PATHB}'"
-    echo "PATHC='${PATHC}'"
-    echo "PATHD='${PATHD}'"
+    ${ECHO} "PATHA='${PATHA}'"
+    ${ECHO} "PATHB='${PATHB}'"
+    ${ECHO} "PATHC='${PATHC}'"
+    ${ECHO} "PATHD='${PATHD}'"
 
-    echo "FILENAMEARG='${FILENAMEARG}'"
-    echo "FILENAME='${FILENAME}'"
-    echo "FILENAMEMAIN='${FILENAMEMAIN}'"
-    echo "FILENAMEEXT='${FILENAMEEXT}'"
+    ${ECHO} "FILENAMEARG='${FILENAMEARG}'"
+    ${ECHO} "FILENAME='${FILENAME}'"
+    ${ECHO} "FILENAMEMAIN='${FILENAMEMAIN}'"
+    ${ECHO} "FILENAMEEXT='${FILENAMEEXT}'"
 
-    echo "FILENAMEMAINFBA='${FILENAMEMAINFBA}'"
-    echo "FILENAMEMAINFBB='${FILENAMEMAINFBB}'"
-    echo "FILENAMEMAINFBC='${FILENAMEMAINFBC}'"
-    echo "FILENAMEMAINFBD='${FILENAMEMAINFBD}'"
+    ${ECHO} "FILENAMEMAINFBA='${FILENAMEMAINFBA}'"
+    ${ECHO} "FILENAMEMAINFBB='${FILENAMEMAINFBB}'"
+    ${ECHO} "FILENAMEMAINFBC='${FILENAMEMAINFBC}'"
+    ${ECHO} "FILENAMEMAINFBD='${FILENAMEMAINFBD}'"
 
-    echo "ARGS='${ARGS}'"
-    echo "ARGA='${ARGA}'"
-    echo "ARGAN='${ARGAN}'"
-    echo "ARGAV='${ARGAV}'"
-    echo "ARGB='${ARGB}'"
-    echo "ARGBN='${ARGBN}'"
-    echo "ARGBV='${ARGBV}'"
-    echo "ARGC='${ARGC}'"
-    echo "ARGCN='${ARGCN}'"
-    echo "ARGCV='${ARGCV}'"
+    ${ECHO} "ARGS='${ARGS}'"
+    ${ECHO} "ARGA='${ARGA}'"
+    ${ECHO} "ARGAN='${ARGAN}'"
+    ${ECHO} "ARGAV='${ARGAV}'"
+    ${ECHO} "ARGB='${ARGB}'"
+    ${ECHO} "ARGBN='${ARGBN}'"
+    ${ECHO} "ARGBV='${ARGBV}'"
+    ${ECHO} "ARGC='${ARGC}'"
+    ${ECHO} "ARGCN='${ARGCN}'"
+    ${ECHO} "ARGCV='${ARGCV}'"
 fi
